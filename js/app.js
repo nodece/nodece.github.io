15938e364e294de01242832e058edbc10cf37e7a
nodece <nodeces@gmail.com>
2017-01-21 23:51:10 +0800

    hexo deploy

---
+++ js/app.js
@@ -0,0 +1,19 @@
+window.onload = function () {
+    //
+    // var top = document.getElementById("back-top");
+    // var screenW = document.documentElement.clientWidth || document.body.clientWidth;
+    // var screenH = document.documentElement.clientHeight || document.body.clientHeight;
+    // top.style.left = screenW - top.offsetWidth + "px";
+    // top.style.top = screenH - top.offsetHeight + "px";
+    // window.onscroll = function () {
+    //     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    //     top.style.top = screenH - top.offsetHeight + scrollTop + "px";
+    // }
+    // top.onclick = function () {
+    //     document.documentElement.scrollTop = document.body.scrollTop = 0;
+    // }
+
+    console.info("你想干啥～")
+}
+
+
